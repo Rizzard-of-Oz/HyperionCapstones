9f3c1f5ee8c164510870651a242e5e34aa7c591b
Rizzard-of-Oz <123083926+Rizzard-of-Oz@users.noreply.github.com>
2023-03-25 22:39:55 +0200

    Update variables.py

--- variables.py
+++ variables.py
@@ -6,3 +6,9 @@ name_of_car_str = input("Which car do you drive? ")
 print(name_of_car_str) # displays user car name
 favorite_movie_str = input("What is your favorite movie? ")
 print(favorite_movie_str) # displays user favourite movie
+ucky_number_int = input("What is your lucky number? ") # enter your lucky number
+print(lucky_number_int) # displays your lucky number
+height_float = input("What is your height in meters? ") # enter your height
+print(height_float) # displays your height
+favorite_color_str = input("What is your favorite color? ") # enter your favourite color
+print(favorite_color_str) # displays your favourite color
